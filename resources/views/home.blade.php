6e8a9f278c61907772ba09f57a176a89f5040e0b
Cetin Mutlu Onal <cetinmutluonal@gmail.com>
2022-09-29 17:40:18 +0300

    Configure home screen for listing authenticated user's todos only

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+<div class="row todoContainer">
+    <div class="col-8">
+        <ul class="list-group">
+            <div class="mb-4" >
+                <form action="{{ route ('todos.create') }}" type="submit" method="GET">
+                    @csrf
+                <button type="submit" class="btn btn-primary float-end"> Create New Todo</button>
+                </form>
             </div>
-        </div>
+                @foreach ($todos as $todo )
+                    @if ($todo->user_id == $user->id)
+                    <li class="list-group-item"><a href="{{ route('todos.show', $todo->id) }}" method="GET">{{ $todo->title }}</a>
+                        <form action="{{ route('todos.destroy', $todo->id) }}" type="submit" method="POST">
+                            @csrf
+                            @method('delete')
+                            <button type="submit" class="btn btn-danger">Delete </button>
+                        </form>
+                    </li>
+                    @endif
+                @endforeach
+        </ul>
     </div>
 </div>
 @endsection
